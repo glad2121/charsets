86fa3c8a144f5ab502df1ac69541ae5a32b3594f
glad2121 <glad2121@gmail.com>
2015-04-19 18:46:07 +0900

    Add JIS X 0213 info.

--- Charsets.java
+++ Charsets.java
@@ -9,6 +9,7 @@ class Charsets {
     static final Charset EUC_JP = Charset.forName("EUC-JP");
     static final Charset SHIFT_JIS = Charset.forName("Shift_JIS");
     static final Charset WINDOWS_31J = Charset.forName("Windows-31J");
+    static final Charset SHIFT_JISX0213 = Charset.forName("x-SJIS_0213");
 
     String option;
     Charset encoding;
@@ -26,7 +27,7 @@ class Charsets {
         } else if ("-euc".equals(option)) {
             return EUC_JP;
         } else if ("-sjis".equals(option)) {
-            return SHIFT_JIS;
+            return SHIFT_JISX0213;
         } else if ("-w31j".equals(option)) {
             return WINDOWS_31J;
         } else if ("-utf8".equals(option)) {
@@ -44,13 +45,13 @@ class Charsets {
         println();
         println("# US-ASCII");
         println();
-        println("         JIS  EUC    SJIS W31J Unicode  UTF-16   UTF-8");
-        println("-------- ---- ------ ---- ---- -------- -------- -------- ----");
+        printHeader();
+        printSeparator();
         for (int c = 0x00; c <= 0x1F; ++c) {
             printLines(encodingLines(c, option));
         }
         List<byte[]> laters = new ArrayList<>();
-        println("-------- ---- ------ ---- ---- -------- -------- -------- ----");
+        printSeparator();
         for (int c = 0x20; c <= 0x7F; ++c) {
             List<byte[]> lines = encodingLines(c, option);
             printLine(lines.get(0));
@@ -62,18 +63,18 @@ class Charsets {
         println();
         println("# JIS X 0201");
         println();
-        println("         JIS  EUC    SJIS W31J Unicode  UTF-16   UTF-8");
-        println("-------- ---- ------ ---- ---- -------- -------- -------- ----");
+        printHeader();
+        printSeparator();
         printLines(laters);
-        println("-------- ---- ------ ---- ---- -------- -------- -------- ----");
+        printSeparator();
         for (int c = 0x80; c <= 0x9F; ++c) {
             printLines(encodingLines(c, option));
         }
-        println("-------- ---- ------ ---- ---- -------- -------- -------- ----");
+        printSeparator();
         for (int c = 0xA0; c <= 0xDF; ++c) {
             printLines(encodingLines(c, option));
         }
-        println("-------- ---- ------ ---- ---- -------- -------- -------- ----");
+        printSeparator();
         for (int c = 0xE0; c <= 0xFF; ++c) {
             printLines(encodingLines(c, option));
         }
@@ -81,22 +82,22 @@ class Charsets {
         println();
         println("# JIS X 0208 - マッピングが異なるもの");
         println();
-        println("   区-点 JIS  EUC    SJIS W31J Unicode  UTF-16   UTF-8");
-        println("-------- ---- ------ ---- ---- -------- -------- -------- ----");
-        printLines(encodingLines(01, 33, option));
-        printLines(encodingLines(01, 34, option));
-        printLines(encodingLines(01, 61, option));
-        printLines(encodingLines(01, 81, option));
-        printLines(encodingLines(01, 82, option));
-        printLines(encodingLines(02, 44, option));
+        printHeaderX0208();
+        printSeparator();
+        printLines(encodingLines(1, 33, option));
+        printLines(encodingLines(1, 34, option));
+        printLines(encodingLines(1, 61, option));
+        printLines(encodingLines(1, 81, option));
+        printLines(encodingLines(1, 82, option));
+        printLines(encodingLines(2, 44, option));
         
         println();
         println("# JIS X 0208 - 非漢字");
         println();
-        println("   区-点 JIS  EUC    SJIS W31J Unicode  UTF-16   UTF-8");
-        for (int k = 01; k <= 12; ++k) {
-            println("-------- ---- ------ ---- ---- -------- -------- -------- ----");
-            for (int t = 01; t <= 94; ++t) {
+        printHeaderX0208();
+        for (int k = 1; k <= 12; ++k) {
+            printSeparator();
+            for (int t = 1; t <= 94; ++t) {
                 printLines(encodingLines(k, t, option));
             }
         }
@@ -104,10 +105,10 @@ class Charsets {
         println();
         println("# NEC特殊文字");
         println();
-        println("   区-点 JIS  EUC    SJIS W31J Unicode  UTF-16   UTF-8");
+        printHeaderX0208();
         for (int k = 13; k <= 15; ++k) {
-            println("-------- ---- ------ ---- ---- -------- -------- -------- ----");
-            for (int t = 01; t <= 94; ++t) {
+            printSeparator();
+            for (int t = 1; t <= 94; ++t) {
                 printLines(encodingLines(k, t, option));
             }
         }
@@ -115,10 +116,10 @@ class Charsets {
         println();
         println("# JIS X 0208 - 第1水準漢字");
         println();
-        println("   区-点 JIS  EUC    SJIS W31J Unicode  UTF-16   UTF-8");
+        printHeaderX0208();
         for (int k = 16; k <= 47; ++k) {
-            println("-------- ---- ------ ---- ---- -------- -------- -------- ----");
-            for (int t = 01; t <= 94; ++t) {
+            printSeparator();
+            for (int t = 1; t <= 94; ++t) {
                 printLines(encodingLines(k, t, option));
             }
         }
@@ -126,10 +127,10 @@ class Charsets {
         println();
         println("# JIS X 0208 - 第2水準漢字");
         println();
-        println("   区-点 JIS  EUC    SJIS W31J Unicode  UTF-16   UTF-8");
+        printHeaderX0208();
         for (int k = 48; k <= 88; ++k) {
-            println("-------- ---- ------ ---- ---- -------- -------- -------- ----");
-            for (int t = 01; t <= 94; ++t) {
+            printSeparator();
+            for (int t = 1; t <= 94; ++t) {
                 printLines(encodingLines(k, t, option));
             }
         }
@@ -137,10 +138,10 @@ class Charsets {
         println();
         println("# NEC選定IBM拡張文字");
         println();
-        println("   区-点 JIS  EUC    SJIS W31J Unicode  UTF-16   UTF-8");
+        printHeaderX0208();
         for (int k = 89; k <= 94; ++k) {
-            println("-------- ---- ------ ---- ---- -------- -------- -------- ----");
-            for (int t = 01; t <= 94; ++t) {
+            printSeparator();
+            for (int t = 1; t <= 94; ++t) {
                 printLines(encodingLines(k, t, option));
             }
         }
@@ -148,10 +149,10 @@ class Charsets {
         println();
         println("# ユーザー外字領域");
         println();
-        println("   区-点 JIS  EUC    SJIS W31J Unicode  UTF-16   UTF-8");
+        printHeaderX0208();
         for (int k = 95; k <= 114; ++k) {
-            println("-------- ---- ------ ---- ---- -------- -------- -------- ----");
-            for (int t = 01; t <= 94; ++t) {
+            printSeparator();
+            for (int t = 1; t <= 94; ++t) {
                 printLines(encodingLines(k, t, option));
             }
         }
@@ -159,13 +160,74 @@ class Charsets {
         println();
         println("# IBM拡張文字");
         println();
-        println("   区-点 JIS  EUC    SJIS W31J Unicode  UTF-16   UTF-8");
+        printHeaderX0208();
         for (int k = 115; k <= 120; ++k) {
-            println("-------- ---- ------ ---- ---- -------- -------- -------- ----");
-            for (int t = 01; t <= 94; ++t) {
+            printSeparator();
+            for (int t = 1; t <= 94; ++t) {
                 printLines(encodingLines(k, t, option));
             }
         }
+        
+        println();
+        println("# JIS X 0213 - 非漢字");
+        println();
+        printHeaderX0213();
+        for (int k = 1; k <= 13; ++k) {
+            if (k == 1) continue;
+            printSeparator();
+            for (int t = 1; t <= 94; ++t) {
+                printLines(encodingLinesX0213(1, k, t, option));
+            }
+        }
+        
+        println();
+        println("# JIS X 0213 - 第3水準漢字");
+        println();
+        printHeaderX0213();
+        for (int k = 14; k <= 94; ++k) {
+            // 第1水準。
+            if (16 <= k && k <= 46) continue;
+            // 第2水準。
+            if (48 <= k && k <= 83) continue;
+            
+            printSeparator();
+            for (int t = 1; t <= 94; ++t) {
+                printLines(encodingLinesX0213(1, k, t, option));
+            }
+        }
+        
+        println();
+        println("# JIS X 0213 - 第4水準漢字");
+        println();
+        printHeaderX0213();
+        for (int k = 1; k <= 94; ++k) {
+            // 補助漢字。
+            if (k == 2) continue;
+            if ( 6 <= k && k <=  7) continue;
+            if ( 9 <= k && k <= 11) continue;
+            if (16 <= k && k <= 77) continue;
+            
+            printSeparator();
+            for (int t = 1; t <= 94; ++t) {
+                printLines(encodingLinesX0213(2, k, t, option));
+            }
+        }
+    }
+
+    void printHeader() {
+        println("         JIS  EUC    SJIS W31J Unicode  UTF-16   UTF-8");
+    }
+
+    void printHeaderX0208() {
+        println("   区-点 JIS  EUC    SJIS W31J Unicode  UTF-16   UTF-8");
+    }
+
+    void printHeaderX0213() {
+        println("面-区-点 JIS  EUC    SJIS W31J Unicode  UTF-16   UTF-8");
+    }
+
+    void printSeparator() {
+        println("-------- ---- ------ ---- ---- -------- -------- ------------ ----");
     }
 
     static final byte[] EMPTY_BYTES = {};
@@ -218,7 +280,7 @@ class Charsets {
         bab.append(String.format("%02X   ", c));
         bab.append(String.format("U+%04X   ", ss.codePointAt(0)));
         bab.append(String.format("%-8s ", toHexString(ss.toCharArray())));
-        bab.append(String.format("%-8s ", toHexString(ss.getBytes(UTF_8))));
+        bab.append(String.format("%-12s ", toHexString(ss.getBytes(UTF_8))));
         if (!ss.equals("\uFFFD")) {
             if (0x20 <= c && c != 0x7F) {
                 bab.append(String.format("[%s]", ss));
@@ -242,7 +304,7 @@ class Charsets {
         bab.append("-    ");
         bab.append(String.format("U+%04X   ", ss.codePointAt(0)));
         bab.append(String.format("%-8s ", toHexString(ss.toCharArray())));
-        bab.append(String.format("%-8s ", toHexString(ss.getBytes(UTF_8))));
+        bab.append(String.format("%-12s ", toHexString(ss.getBytes(UTF_8))));
         if (!ss.equals("\uFFFD")) {
                 bab.append(String.format("[%s]", ss));
             byte[] bs2 = ss.getBytes(SHIFT_JIS);
@@ -254,8 +316,8 @@ class Charsets {
     }
 
     List<byte[]> encodingLines(int k, int t, String option) {
-        int jis = kutenToJis(k, t);
-        int euc = kutenToEuc(k, t);
+        int jis = (94 < k) ? 0 : kutenToJis(k, t);
+        int euc = (94 < k) ? 0 : kutenToEuc(k, t);
         int sjis = kutenToSjis(k, t);
         byte[] bs = bytes(sjis, 2);
         String ss = toString(bs, SHIFT_JIS);
@@ -273,8 +335,8 @@ class Charsets {
             bab.append("-    ");
             bab.append(String.format("U+%04X   ", ss.codePointAt(0)));
             bab.append(String.format("%-8s ", toHexString(ss.toCharArray())));
-            bab.append(String.format("%-8s ", toHexString(ss.getBytes(UTF_8))));
-            if ("-sjis".equals(option) || "-w31j".equals(option)) {
+            bab.append(String.format("%-12s ", toHexString(ss.getBytes(UTF_8))));
+            if ("-w31j".equals(option)) {
                 bab.append("[").append(bs).append("]");
             } else {
                 bab.append(String.format("[%s]", ss));
@@ -307,9 +369,9 @@ class Charsets {
         bab.append(String.format("%04X ", sjis));
         bab.append(String.format("U+%04X   ", sw.codePointAt(0)));
         bab.append(String.format("%-8s ", toHexString(sw.toCharArray())));
-        bab.append(String.format("%-8s ", toHexString(sw.getBytes(UTF_8))));
+        bab.append(String.format("%-12s ", toHexString(sw.getBytes(UTF_8))));
         if (!sw.equals("\uFFFD")) {
-            if ("-sjis".equals(option) || "-w31j".equals(option)) {
+            if ("-w31j".equals(option)) {
                 bab.append("[").append(bs).append("]");
             } else {
                 bab.append(String.format("[%s]", sw));
@@ -317,10 +379,10 @@ class Charsets {
             byte[] bw2 = sw.getBytes(WINDOWS_31J);
             if (!Arrays.equals(bs, bw2)) {
                 bab.append(String.format(" => %s", toHexString(bw2)));
-            } else if (showSjis) {
-                byte[] bs2 = ss.getBytes(SHIFT_JIS);
+            } else {
+                byte[] bs2 = sw.getBytes(SHIFT_JISX0213);
                 if (!Arrays.equals(bs, bs2)) {
-                    bab.append(String.format(" -> %s (SJIS)", toHexString(bs2)));
+                    bab.append(String.format(" -> %s (SJIS0213)", toHexString(bs2)));
                 }
             }
         }
@@ -329,18 +391,118 @@ class Charsets {
         return lines;
     }
 
+    List<byte[]> encodingLinesX0213(int m, int k, int t, String option) {
+        // 補助漢字。
+        if (m == 2 && (k == 2
+                || ( 6 <= k && k <=  7)
+                || ( 9 <= k && k <= 11)
+                || (16 <= k && k <= 77))) {
+            return Collections.emptyList();
+        }
+        
+        int jis = kutenToJis(k, t);
+        int euc = kutenToEuc(m, k, t);
+        int sjis = kutenToSjis(m, k, t);
+        byte[] bs = bytes(sjis, 2);
+        String sx = toString(bs, SHIFT_JISX0213);
+        String ss = toString(bs, SHIFT_JIS);
+        String sw = toString(bs, WINDOWS_31J);
+        
+        if (sx.charAt(0) == '\uFFFD' || sx.equals(ss)) {
+            return Collections.emptyList();
+        }
+        
+        ByteArrayBuilder bab = new ByteArrayBuilder();
+        bab.append(String.format("%2d-%02d-%02d ", m, k, t));
+        bab.append(String.format("%04X ", jis));
+        if (euc <= 0xFFFF) {
+            bab.append(String.format("%04X   ", euc));
+        } else {
+            bab.append(String.format("%06X ", euc));
+        }
+        bab.append(String.format("%04X ", sjis));
+        if (!sw.equals(sx)) {
+            bab.append("-    ");
+        } else {
+            bab.append(String.format("%04X ", sjis));
+        }
+        if (sx.codePointCount(0, sx.length()) > 1) {
+            bab.append("-        ");
+        } else {
+            int cpx = sx.codePointAt(0);
+            if (cpx <= 0xFFFF) {
+                bab.append(String.format("U+%04X   ", cpx));
+            } else {
+                bab.append(String.format("U+%06X ", cpx));
+            }
+        }
+        bab.append(String.format("%-8s ", toHexString(sx.toCharArray())));
+        bab.append(String.format("%-12s ", toHexString(sx.getBytes(UTF_8))));
+        bab.append(String.format("[%s]", sx));
+        byte[] bx2 = sx.getBytes(SHIFT_JISX0213);
+        if (!Arrays.equals(bs, bx2)) {
+            bab.append(String.format(" => %s", toHexString(bx2)));
+        } else {
+            byte[] bw2 = sx.getBytes(WINDOWS_31J);
+            if (!Arrays.equals(bs, bw2)) {
+                bab.append(String.format(" -> %s (W31J)", toHexString(bw2)));
+            }
+        }
+        return Collections.singletonList(bab.toByteArray());
+    }
+
     static int kutenToJis(int k, int t) {
+        assertRange("k", k, 1, 94);
+        assertRange("t", t, 1, 94);
         return word(k + 0x20, t + 0x20);
     }
 
     static int kutenToEuc(int k, int t) {
+        assertRange("k", k, 1, 94);
+        assertRange("t", t, 1, 94);
         return word(k + 0xA0, t + 0xA0);
     }
 
+    static int kutenToEuc(int m, int k, int t) {
+        assertRange("m", m, 1, 2);
+        if (m == 1) {
+            return kutenToEuc(k, t);
+        }
+        return 0x8F0000 | kutenToEuc(k, t);
+    }
+
     static int kutenToSjis(int k, int t) {
-        int c1 = (k - 1) / 2 + (k <= 62 ? 0x81 : 0xC1);
+        assertRange("k", k, 1, 120);
+        assertRange("t", t, 1, 94);
+        int c1 = (k + (k <= 62 ? 0x101 : 0x181)) / 2;
         int c2 = t + (k % 2 == 1 ? (t <= 63 ? 0x3F : 0x40) : 0x9E);
         return word(c1, c2);
+    }
+
+    static int kutenToSjis(int m, int k, int t) {
+        assertRange("m", m, 1, 2);
+        assertRange("k", k, 1, 94);
+        if (m == 1) {
+            return kutenToSjis(k, t);
+        }
+        
+        if (k == 2
+                || ( 6 <= k && k <=  7)
+                || ( 9 <= k && k <= 11)
+                || (16 <= k && k <= 77)) {
+            throw new IllegalArgumentException("m = 2, k = " + k);
+        }
+        int c1 = (k <= 15)
+            ? ((k + 0x1DF) / 2 - (k / 8) * 3)
+            : ((k + 0x19B) / 2);
+        int c2 = t + (k % 2 == 1 ? (t <= 63 ? 0x3F : 0x40) : 0x9E);
+        return word(c1, c2);
+    }
+
+    static void assertRange(String name, int value, int min, int max) {
+        if (value < min || max < value) {
+            throw new IllegalArgumentException(name + " = " + value);
+        }
     }
 
     static int kuten(int k, int t) {
